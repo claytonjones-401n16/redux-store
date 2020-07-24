0bbb5480c8bdfbbc214015bfec860c42267d2341
Clayton Jones <claytonjonesLMP@gmail.com>
2020-07-23 21:15:34 -0700

    product details page display finished

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -1,6 +1,18 @@
 import React, {useEffect} from 'react';
 import {connect} from 'react-redux';
 import { getOneProduct } from '../store/products-actions';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
+import {
+  Grid, 
+  Card,
+  CardContent, 
+  Accordion, 
+  AccordionDetails, 
+  AccordionSummary, 
+  Typography,
+  Button
+} from '@material-ui/core';
 
 
 function ProductDetails(props) {
@@ -12,11 +24,127 @@ function ProductDetails(props) {
     getOneProduct(id);
   }, [getOneProduct, id]);
 
+  const styles = {
+    gridContainer: {
+      width: '50%',
+      margin: 'auto'
+    },
+    title: {
+      textAlign: 'center'
+    },
+    stock: {
+      textAlign: 'start'
+    },
+    price: {
+      textAlign: 'end'
+    }
+  }
 
   return (
-    <>
-      {productToView.name}
-    </>
+      <Grid container spacing={2} justify='center' alignItems='center' style={styles.gridContainer}>
+        <Grid item xs={12}>
+          <Card style={styles.card}>
+            <CardContent>
+              <Grid container spacing={3} justify='center' alignItems='center'>
+                <Grid item xs={12}>
+                  <Typography color='textPrimary' variant='h1' component='h2' gutterBottom style={styles.title}>
+                    {productToView.name}
+                  </Typography>
+                </Grid>
+                <Grid item xs={12}>
+                  <Typography color='textSecondary' variant='body1' gutterBottom>
+                    {productToView.description}
+                  </Typography>
+                </Grid>
+                <Grid item xs={6}>
+                  <Typography color='textSecondary' variant='body1' style={styles.stock}>
+                    {`Stock: ${productToView.stock}`}
+                  </Typography>
+                </Grid>
+                <Grid item xs={6}>
+                  <Typography color='textSecondary' variant='body1' style={styles.price}>
+                    ${productToView.price}
+                  </Typography>
+                </Grid>
+              </Grid>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={12}>
+          <Button color='primary' fullWidth='true' variant='contained'>BUY</Button>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant='h5'>
+            Related Items
+          </Typography>
+        </Grid>
+        <Grid item xs={4}>
+          <Card>
+            <CardContent>
+              <Typography variant='body2' display='inline' align='center'>
+                Suggested Item
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={4}>
+          <Card>
+            <CardContent>
+              <Typography variant='body2' display='inline' align='center'>
+                Suggested Item
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={4}>
+          <Card>
+            <CardContent>
+              <Typography variant='body2' display='inline' align='center'>
+                Suggested Item
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant='h5'>
+            Product Details
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Accordion>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1a-content"
+              id="panel1a-header"
+            >
+              <Typography variant='body2'>
+                Specifications
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography variant='body3' color='textSecondary'>
+                Sample Specs
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+          <Accordion>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1a-content"
+              id="panel1a-header"
+            >
+              <Typography variant='body2'>
+                User Reviews
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography variant='body3' color='textSecondary'>
+                Sample Reviews
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+        </Grid>
+      </Grid>
   )
 }
 
